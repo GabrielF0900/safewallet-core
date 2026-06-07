db386d4252783462f3b19c97458485198f31931b
GabrielF0900 <gabrielcfonline0900@gmail.com>
2026-06-07 14:33:40 -0300

    Finalizando a documentação da API e adicionando autenticação via Token JWT

---
+++ backend/src/main/java/br/com/safewallet/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package br.com.safewallet.config;
+//CLASSE DE AUTENTICAÇÃO PARA USAR A DOCUMENTAÇÃO.
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .name("BearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+    
+}
